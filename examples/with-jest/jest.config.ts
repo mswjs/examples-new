abee1e0c642f69d78cad50e95af2ff22f8a4a0c6
Artem Zakharchenko <kettanaito@gmail.com>
2023-04-28 13:06:52 +0200

    add jest jsdom test case

--- examples/with-jest/jest.config.ts
+++ examples/with-jest/jest.config.ts
@@ -6,4 +6,16 @@ export default {
   transform: {
     '^.+\\.tsx?$': '@swc/jest',
   },
+  testEnvironmentOptions: {
+    /**
+     * @note Opt-out from JSDOM using browser-style resolution
+     * for dependencies. This is simply incorrect, as JSDOM is
+     * not a browser, and loading browser-oriented bundles in
+     * Node.js will break things.
+     *
+     * Consider migrating to a more modern test runner if you
+     * don't want to deal with this.
+     */
+    customExportConditions: [''],
+  },
 } satisfies Config
