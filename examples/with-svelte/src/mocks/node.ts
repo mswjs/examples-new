99d1c6cd6249b4d2d7da975d8f59d85038e44818
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-05-05 14:34:30 +0200

    Remove pure annotation

--- examples/with-svelte/src/mocks/node.ts
+++ examples/with-svelte/src/mocks/node.ts
@@ -1,4 +1,4 @@
 import { setupServer } from 'msw/node';
 import { handlers } from './handlers';
 
-export const server = /*#__PURE__*/ setupServer(...handlers);
+export const server = setupServer(...handlers);
